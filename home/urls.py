ec4c865daf6f5acaaaee8c306b64f1028d5f1241
Atsushi <todaatsushi0@gmail.com>
2020-03-29 17:14:29 +0100

    Refactor into CBV

--- home/urls.py
+++ home/urls.py
@@ -4,11 +4,11 @@ from home import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('', views.Home.as_view(), name='home'),
+    path('about/', views.About.as_view(), name='about'),
     path('contact/', views.Contact.as_view(), name='contact'),
 
-    path('projects/', views.ViewProjects.as_view(), name='all_projects'),
+    path('projects/', views.ViewAllProjects.as_view(), name='all_projects'),
     path('projects/<str:slug>/', views.ViewProject.as_view(), name='view_project'),
     path('projects/<str:slug>/<int:pk>/', views.ViewPhoto.as_view(), name='view_photo'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,29 +1,41 @@
 from django.shortcuts import render
-from django.views.generic import (ListView, DetailView, FormView)
+from django.views.generic import View
 
 from home import models
 
 
-def home(request):
-    projects = models.Project.objects.filter(active=True)
-    return render(request, 'home/home.html', {'projects': projects})
+class Home(View):
+    def get(self, request):
+        projects = models.Project.objects.filter(active=True)
+        return render(request, 'home/home.html', {'projects': projects})
 
 
-def about(request):
-    pass
+class About(View):
+    def get(self, request):
+        projects = models.Project.objects.filter(active=True)
+        return render(request, 'home/about.html', {'projects': projects})
 
 
-class Contact(FormView):
-    pass
+class Contact(View):
+    def get(self, request):
+        projects = models.Project.objects.filter(active=True)
+        return render(request, 'home/contact.html', {'projects': projects})
 
 
-class ViewProjects(ListView):
-    pass
+# TODO - SPLIT INTO PROJECTS APP
+class ViewAllProjects(View):
+    def get(self, request):
+        projects = models.Project.objects.filter(active=True)
+        return render(request, 'home/home.html', {'projects': projects})
 
 
-class ViewProject(DetailView):
-    pass
+class ViewProject(View):
+    def get(self, request):
+        projects = models.Project.objects.filter(active=True)
+        return render(request, 'home/home.html', {'projects': projects})
 
 
-class ViewPhoto(DetailView):
-    pass
+class ViewPhoto(View):
+    def get(self, request):
+        projects = models.Project.objects.filter(active=True)
+        return render(request, 'home/home.html', {'projects': projects})
