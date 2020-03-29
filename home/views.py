62d69a2c46d6526729e246aef8103f565ce3d6ca
Atsushi <todaatsushi0@gmail.com>
2020-03-29 16:59:54 +0100

    Add view and url frameworks

--- home/views.py
+++ home/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import (ListView, DetailView, FormView)
 
-# Create your views here.
+from home import models
+
+
+def home(request):
+    projects = models.Project.objects.filter(active=True)
+    return render(request, 'home/home.html', {'projects': projects})
+
+
+def about(request):
+    pass
+
+
+class Contact(FormView):
+    pass
+
+
+class ViewProjects(ListView):
+    pass
+
+
+class ViewProject(DetailView):
+    pass
+
+
+class ViewPhoto(DetailView):
+    pass
--- npp/urls.py
+++ npp/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('/', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
