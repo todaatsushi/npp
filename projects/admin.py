4e40e324dff2d9e0c7a44c1e94b5840936049d2b
Atsushi <todaatsushi0@gmail.com>
2020-04-05 19:40:43 +0100

    Add admin actions to manage active

--- projects/admin.py
+++ projects/admin.py
@@ -3,20 +3,50 @@ from django.contrib import admin
 from projects import models
 
 
-class ProjectAdmin(admin.ModelAdmin):
+class ManageActiveMixin:
+
+    actions = ['activate', 'deactivate']
+
+    def activate(self, request, queryset):
+        for instance in queryset:
+            instance.active = True
+            instance.save()
+        self.message_user(
+            request, 'Successfully activated instances.'
+        )
+
+    def deactivate(self, request, queryset):
+        for instance in queryset:
+            instance.active = False
+            instance.save()
+        self.message_user(
+            request, 'Successfully deactivated instances.'
+        )
+
+    activate.short_description = (
+        'Turn on \'active\' of all selected.'
+    )
+
+    deactivate.short_description = (
+        'Turn off \'active\' of all selected.'
+    )
+
+
+class ProjectAdmin(ManageActiveMixin, admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': ['name', 'year'],
+            'fields': ['name', 'year', 'active'],
         }),
         ('Meta', {
             'fields': ['date_added', 'last_updated'],
         })
     )
-    list_display = ['name', 'year', 'date_added', 'last_updated']
+    list_display = ['name', 'year', 'date_added', 'last_updated', 'active']
     list_filter = ['year']
 
 
-class PhotoAdmin(admin.ModelAdmin):
+
+class PhotoAdmin(ManageActiveMixin, admin.ModelAdmin):
     fieldsets = (
         (None, {
             'fields': ['name', 'upload_url', 'project__name',],
