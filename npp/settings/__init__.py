a7849d2eda27788bf870c931e95faeba78c84587
Atsushi <todaatsushi0@gmail.com>
2020-04-11 23:17:34 +0100

    Split settings & Update gitignore

---
+++ npp/settings/__init__.py
@@ -0,0 +1,4 @@
+from .common import *
+from .apps import *
+from .static import *
+from .integrations import *
---
+++ npp/settings/integrations.py
@@ -0,0 +1,5 @@
+import os
+
+# IMGUR
+IMGUR_CLIENT_ID = os.environ.get('IMGUR_CLIENT_ID')
+IMGUR_SECRET_KEY = os.environ.get('IMGUR_SECRET_KEY')
