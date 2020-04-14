d35fbd5e6ab39936908c3122e5fd53c50ad4c0ea
Atsushi <todaatsushi0@gmail.com>
2020-04-14 12:27:55 +0100

    Add security

--- npp/settings/common.py
+++ npp/settings/common.py
@@ -8,12 +8,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ib)62x3)qg4j3$gg3y4=yy(tzdt@2ol-s4b7xx#oj@dt06yl_j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
