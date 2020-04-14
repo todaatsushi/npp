1de37f76dcf52ed38eb979716d313afd62208fc0
Atsushi <todaatsushi0@gmail.com>
2020-04-14 18:17:06 +0100

    Remove static files and move static and media folders to root

--- npp/settings/common/all.py
+++ npp/settings/common/all.py
@@ -2,6 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.abspath(os.path.dirname(__name__))
 
 
 # Quick-start development settings - unsuitable for production
--- npp/settings/common/static.py
+++ npp/settings/common/static.py
@@ -1,13 +1,13 @@
 import os
 
-from . import BASE_DIR
+from . import PROJECT_DIR
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Uploaded images
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 MEDIA_URL = '/media/'
