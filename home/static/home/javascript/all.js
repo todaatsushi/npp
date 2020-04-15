42c250cfedbf556f5d7927ed6d91842bf98a36ea
Atsushi <todaatsushi0@gmail.com>
2020-04-15 20:41:16 +0100

    Split into mobile and desktop and make changes

---
+++ home/static/home/javascript/all.js
@@ -0,0 +1,11 @@
+jQuery(function($) {
+  $('#hide-show').click(function() {
+    $('.projects').slideToggle('slow');
+  });
+});
+
+jQuery(function($) {
+  $('#trigger').click(function() {
+    $('.projects').slideToggle('slow');
+  });
+});
